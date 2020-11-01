a27f6b90687968bb27dbf5393b8860e03b3eafde
Gerald Versluis <gerald@verslu.is>
2020-11-01 15:54:05 +0100

    Implement Preferences

--- XFEPreferencesSample/MainPage.xaml.cs
+++ XFEPreferencesSample/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Essentials;
 
 namespace XFEPreferencesSample
 {
@@ -13,6 +14,20 @@ namespace XFEPreferencesSample
         public MainPage()
         {
             InitializeComponent();
+
+            RandomLabel.Text = Preferences.Get("RandomText", string.Empty);
+            RandomSwitch.IsToggled = Preferences.Get("RandomSwitch", false);
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Preferences.Set("RandomText", RandomLabel.Text);
+            Preferences.Set("RandomSwitch", RandomSwitch.IsToggled);
+        }
+
+        void Button_Clicked_1(System.Object sender, System.EventArgs e)
+        {
+            Preferences.Clear();
         }
     }
 }
